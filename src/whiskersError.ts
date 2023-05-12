dd111682c17266338c1c24fda26e525aeec891db
dtregea <daniel.tregea@gmail.com>
2023-05-12 19:15:35 -0400

    Removed request and response from whiskers error

--- src/whiskersError.ts
+++ src/whiskersError.ts
@@ -4,30 +4,26 @@ import { AxiosError, AxiosResponse } from "axios";
  * This class provides additional properties to give more context about errors.
  */
 export class WhiskersError extends Error {
-    /** The HTTP request that was made. */
-    public request: any;
-    /** The HTTP response that was received. This is null if no response was received. */
-    public response: AxiosResponse | null;
-    /** The HTTP status of the response. This is undefined if no response was received. */
-    public status: number | undefined;
+    /** The response data of the HTTP response.*/
+    public detail: string;
     /** The URL of the HTTP request. This is undefined if no response was received. */
-    public url: string | undefined;
-    /** The response data (body) of the HTTP response. This is undefined if no response was received. */
-    public detail: string | undefined;
-
+    public url: string | null;
+    /** The HTTP status of the response. This is undefined if no response was received. */
+    public status: number | null;
+    
     /**
      * The constructor for the WhiskersError class.
      * @param {AxiosError} error - The AxiosError object from which to create the WhiskersError.
      */
     constructor(error: AxiosError) {
         super(error.message);
-        this.request = error.request;
-        this.response = error.response ?? null;
-        if (this.response) {
-            this.detail = this.response.data;
-            this.status = this.response.status;
-            this.url = this.response.config.url;
+        this.detail = error.message;
+        this.url = error.config.url ?? null
+        this.status = null;
+        if (error.response) {
+            this.status = error.response.status;
+            this.detail = error.response.data as string ?? null
         }
-        Object.setPrototypeOf(this, WhiskersError.prototype);                        
+        Object.setPrototypeOf(this, WhiskersError.prototype);
     }
 }
